8632f26a6f16ecfa763fb98f91c3b722c3949fbf
Nikola Bogicevic <nbogicevic2@gmail.com>
2021-07-26 17:46:00 -0400

    first commit

---
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -0,0 +1,21 @@
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
+
+import "./ExpenseItem.css";
+
+function ExpenseItem({ date, title, amount }) {
+
+
+  return (
+    <>
+      <div className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2 className="expense-item__title">{title}</h2>
+          <div className="expense-item__price">${amount}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ExpenseItem;
